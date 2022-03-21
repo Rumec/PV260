4e3d42798a0f010ccad99f0818f943832e9e798e
YSOFT\seckar <David.Seckar@ysoft.com>
2022-03-21 08:37:38 +0100

    Created models

---
+++ PV260_Project/DAL/Models/CsvFile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    public class CsvFile
+    {
+        public DateTime Date { get; set; }
+        public List<Holding> Holdings { get; set; } = new List<Holding>();
+
+    }
+}
---
+++ PV260_Project/DAL/Models/Holding.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    public class Holding
+    {
+        public string Fund { get; set; }
+        public string Company { get; set; }
+        public string Ticker { get; set; }
+        public string Shares { get; set; }
+        public double MarketValue { get; set; }
+        public double Weight { get; set; }
+    }
+}
