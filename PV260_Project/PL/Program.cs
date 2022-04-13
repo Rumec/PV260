8dc962b4335f6a4587586d8f1f15959a92264ab8
Martin Bučo <darthersidious@gmail.com>
2022-04-13 16:34:22 +0200

    feat: added console program to run the program

--- PV260_Project/PL/Program.cs
+++ PV260_Project/PL/Program.cs
@@ -1,5 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using DAL;
+using System.Globalization;
+using BL.DataLoading;
+using BL.DiffComputing;using BL.Writers;
 
-Console.WriteLine("Hello world!");
+
+if (Environment.GetCommandLineArgs().Length < 4 || Environment.GetCommandLineArgs().Length > 5)
+{
+    Console.WriteLine("Invalid number of arguments, must be 4 or 5!");
+    return;
+}
+
+string inputFile = Environment.GetCommandLineArgs()[1];
+string sndFile = Environment.GetCommandLineArgs()[2];
+string delim = Environment.GetCommandLineArgs()[3];
+string? outputFile = Environment.GetCommandLineArgs().Length == 5 ?
+    Environment.GetCommandLineArgs()[4] : null;
+
+
+if (!File.Exists(inputFile) || !File.Exists(sndFile))
+{
+    Console.WriteLine("Files don't exist!");
+    return;
+}
+
+// TODO Take this from cmd line
+var csvFileLoader = new CsvFileLoader(delim);
+var fstParsedFile = csvFileLoader.LoadCsvFile(inputFile);
+var sndParsedFile = csvFileLoader.LoadCsvFile(sndFile);
+
+var diffComputer = new DiffComputer();
+var changes = diffComputer.ComputeDiff(fstParsedFile, sndParsedFile);
+
+new ConsoleResultWriter(new CultureInfo("en-US"), DateTime.Now, delim).Print(changes);
