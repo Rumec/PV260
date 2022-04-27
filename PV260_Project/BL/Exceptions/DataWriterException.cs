8583854dd13dc89395fc6cb31913d9133db3eb3f
tompsota <to.psota@gmail.com>
2022-04-27 11:01:19 +0200

    feat: added custom writing exception

---
+++ PV260_Project/BL/Exceptions/DataWriterException.cs
@@ -0,0 +1,11 @@
+namespace BL.Exceptions;
+
+[Serializable]
+public class DataWriterException : Exception
+{
+    public DataWriterException() {}
+
+    public DataWriterException(string message) : base(message) {}
+    
+    public DataWriterException(string message, Exception innerException) : base(message, innerException) {}
+}
