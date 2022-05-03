e61b272651cb2954d2ed9580963d22a37493b8e5
Rumec <drumian@seznam.cz>
2022-05-03 18:29:10 +0200

    Update CsvFileLoader.cs

--- PV260_Project/BusinessLayer/DataLoading/CsvFileLoader.cs
+++ PV260_Project/BusinessLayer/DataLoading/CsvFileLoader.cs
@@ -11,21 +11,21 @@ namespace BusinessLayer.DataLoading;
 public class CsvLine
 {
     [Name("date")]
-    public string? Date { get; set; }
+    public string Date { get; set; }
     [Name("fund")]
-    public string? Fund { get; set; }
+    public string Fund { get; set; }
     [Name("company")]
-    public string? Company { get; set; }
+    public string Company { get; set; }
     [Name("ticker")]
-    public string? Ticker { get; set; }
+    public string Ticker { get; set; }
     [Name("cusip")]
-    public string? Cusip { get; set; }
+    public string Cusip { get; set; }
     [Name("shares")]
-    public string? Shares { get; set; }
+    public string Shares { get; set; }
     [Name("market value ($)")]
-    public string? MarketValue { get; set; }
+    public string MarketValue { get; set; }
     [Name("weight (%)")]
-    public string? Weight { get; set; }
+    public string Weight { get; set; }
 }
 
 public class CsvFileLoader : IDataLoader
