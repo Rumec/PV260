3daa4d3c9378d73f4f3c93296340d0e6976e897d
YSOFT\seckar <David.Seckar@ysoft.com>
2022-05-04 19:40:36 +0200

    added DI

--- PV260_Project/PresentationLayer/Program.cs
+++ PV260_Project/PresentationLayer/Program.cs
@@ -1,13 +1,36 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using PresentationLayer.ConsoleApps;
 
 namespace PresentationLayer
 {
     public class Program
     {
-        public static void Main()
-        {
+        private readonly IApp _app;
+
+        public static void Main() {
+            var args = Environment.GetCommandLineArgs();
+            var host = CreateHostBuilder(args).Build();
+            host.Services.GetRequiredService<Program>().Run();
             var consoleApp = new ConsoleApp();
             consoleApp.Run();
+        }
+
+        public Program(IApp app) {
+            _app = app;
+        }
+
+        public void Run() {
+            _app.Run();
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args) {
+            return Host.CreateDefaultBuilder(args).ConfigureServices(
+                services =>
+                {
+                    services.AddTransient<Program>();
+                    services.AddTransient<IApp, ConsoleApp>();
+                });
         }
     }
 }
