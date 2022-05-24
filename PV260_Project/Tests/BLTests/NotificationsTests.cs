de5e00e98aeb1e91becfbbf56f018d0a322dcbba
Martin Bučo <58446037+DarthXerox@users.noreply.github.com>
2022-05-25 00:34:57 +0200

    fix:deleted useless attributes

--- PV260_Project/Tests/BLTests/NotificationsTests.cs
+++ PV260_Project/Tests/BLTests/NotificationsTests.cs
@@ -17,18 +17,6 @@ namespace Tests.BLTests;
 public class NotificationsTests
 {
     private List<HoldingChanges> _changes = new List<HoldingChanges>();
-    private CultureInfo _culture;
-
-
-    private string _holdingsStr =
-        "date,fund,company,ticker,cusip,shares,\"shares difference\",\"market value difference ($)\",\"weight difference (%)\"" +
-        Environment.NewLine +
-        "04/12/2022,ARKK,\"TESLA INC\",TSLA,88160R101,\"1,356,326\",\"10.00\",\"$1,228,003,997.14\",9.66%" +
-        Environment.NewLine +
-        "04/12/2022,ARKK,\"TELADOC HEALTH INC\",TDOC,87918A105,\"12,395,542\",\"-20.00\",\"$854,796,576.32\",-6.72%" +
-        Environment.NewLine +
-        "04/12/2022,ARKK,\"ROKU INC\",ROKU,77543R102,\"6,452,422\",\"5.00\",\"$805,584,886.70\",6.34%" +
-        Environment.NewLine;
 
     // The password must be filled in!!!
     // You can find it in our discord
@@ -52,8 +40,6 @@ public class NotificationsTests
         _changes.Add(TestUtils.CreateTeslaHoldingChanges());
         _changes.Add(TestUtils.CreateHealthHoldingChanges());
         _changes.Add(TestUtils.CreateRokuHoldingChanges());
-
-        _culture = new CultureInfo("en-US");
 
         _mailOptions = Options.Create(_mailSettings);
         _msgBuilder = new HtmlMessageBuilder();
